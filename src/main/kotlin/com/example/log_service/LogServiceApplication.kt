4478b9aa1086895b4c9da452e6bef1578461b8e8
mohammed shareef <mohammadshariffethi@gmail.com>
2026-08-09 21:26:54 +0300

    starter commit: project init

---
+++ src/main/kotlin/com/example/log_service/LogServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.log_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogServiceApplication>(*args)
+}
